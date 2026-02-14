6f2a28320f558bc0b54742eb17eb3a30d22ff961
Adebimpe12 <156775119+Adebimpe12@users.noreply.github.com>
2026-02-14 17:13:51 +0100

    Combine yellow and green trip data with union

--- models/intermediate/int_trips_unioned.sql
+++ models/intermediate/int_trips_unioned.sql
@@ -1,1 +1,11 @@
+with yellow as (
+    select * from {{ ref('stg_yellow_tripdata') }}
+),
+green as (
+    select * from {{ ref('stg_green_tripdata') }}
+)
+
+select * from yellow
+union all
+select * from green
 
