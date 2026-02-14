12bcb0ea1f802d57d0365899feaf7f3d6275d393
Adebimpe12 <156775119+Adebimpe12@users.noreply.github.com>
2026-02-14 17:15:52 +0100

    Add safe_cast macro for conditional casting

--- macros/safe_cast.sql
+++ macros/safe_cast.sql
@@ -1,1 +1,8 @@
 
+{% macro safe_cast(column, data_type) %}
+    {% if target.type == 'bigquery' %}
+        safe_cast({{ column }} as {{ data_type }})
+    {% else %}
+        cast({{ column }} as {{ data_type }})
+    {% endif %}
+{% endmacro %}
